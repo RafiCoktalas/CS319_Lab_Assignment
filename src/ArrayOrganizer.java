ecf453510e3c00afb002f6a89d1e9d0dfaa24884
raficoktalas <rafi.coktalas@ug.bilkent.edu.tr>
2020-04-07 11:19:14 +0300

    I've fogotten to add the third list

--- src/ArrayOrganizer.java
+++ src/ArrayOrganizer.java
@@ -7,6 +7,7 @@ public class ArrayOrganizer {
         ArrayList<Integer> result = new ArrayList<>();
         result.addAll(list1);
         result.addAll(list2);
+	result.addAll(list3);
         Collections.sort(result, Collections.reverseOrder());
         return result;
     }
